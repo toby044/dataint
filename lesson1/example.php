a36b31e2544bde4e697e3b9bd735d9a1bec1d0c7
toby044 <tobiasnolesen@gmail.com>
2022-02-27 22:55:46 +0100

    XML Testing

--- lesson1/example.php
+++ lesson1/example.php
@@ -1,19 +1,44 @@
 <?php
-$skills = simplexml_load_file('data.xml');
+$xml = simplexml_load_file('data.xml');
 
-echo '<h1>My skills</h1>' . '<br>';
-echo $skills->codingskills->skill[0]->language;
-echo '<br>';
-echo $skills->codingskills->skill[0]->description;
-echo '<br>';
-echo $skills->codingskills->skill[0]->experience;
-echo '<br>';
-echo '<br>';
+$skills = $xml->skill;
 
-echo $skills->codingskills->skill[1]->language;
-echo '<br>';
-echo $skills->codingskills->skill[1]->description;
-echo '<br>';
-echo $skills->codingskills->skill[1]->experience;
-echo '<br>';
-?>
+
+echo '<h1>My skills</h1>';
+
+foreach($skills as $data) {
+    foreach($data->language as $txt){
+        echo $txt.'<br>';    
+    }
+    foreach($data->program as $txt){
+        echo $txt.'<br>';    
+    }
+    foreach($data->description as $txt){
+        echo $txt.'<br>';    
+    }
+    foreach($data->experience as $txt){
+        echo $txt.'<br>';    
+    }
+    
+    echo '<br>';
+}
+
+
+echo '<br>' . '<p>This is made with data from XML: eXtensible Markup Language</p>';
+
+echo '<h2>Pro / Cons of XML</h2>';
+echo '<h3>Pros</h3>';
+echo '<ul>';
+echo '<li>Independant programming language</li>';
+echo '<li>Supports unicode</li>';
+echo '<li>Liquid data changing. If you change data in your XML file, it will automatically update because other languages like html will draw directly from the data file</li>';
+echo '<li>Data validation with DTD, Schema or VSCode extensions</li>';
+echo '</ul>';
+
+echo '<h3>Cons</h3>';
+echo '<ul>';
+echo '<li>Redundant compared to JSON</li>';
+echo '<li>Higher storage because of the redundant syntax</li>';
+echo '<li>Less readable than other text-based data transmissions like JSON</li>';
+echo '<li>XML files can get big because of the nature of the syntax</li>';
+echo '</ul>';
